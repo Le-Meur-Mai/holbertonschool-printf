520851a1f83734e57852d1d36834acbf237a0fcb
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 <11534@holbertonstudents.com>
2025-07-23 10:49:16 +0200

    fix print of "%"

--- _printf.c
+++ _printf.c
@@ -41,6 +41,8 @@ int _printf(const char *format, ...)
 			else
 				count += _putchar('%');
 		}
+		else if (format[i] == '%' && format[i + 1] == '\0')
+			return (-1);
 		else
 		count += _putchar(format[i]);
 	}
